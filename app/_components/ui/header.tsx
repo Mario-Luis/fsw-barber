bc609e721415f891deed4b660768fabc47d51ccf
Mario Luis <yamamotomarioluis@gmail.com>
2024-08-11 14:38:31 +0900

    feat: add Header components and banner

---
+++ app/_components/ui/header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { Card,CardContent } from "./card";
+import { Button } from "./button";
+import { MenuIcon } from "lucide-react";
+
+
+const Header = () => {
+    return (
+        <Card>
+            <CardContent className="p-5 flex flex-row justify-between items-center ">
+                <Image alt="FSW-Barber" src="/logo.png" height={18} width={120} />
+                <Button size="icon" variant="outline">
+                    <MenuIcon />
+                </Button>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
+import { SearchIcon } from "lucide-react";
 import { Button } from "./_components/ui/button"
-
+import Header from "./_components/ui/header"
+import { Input } from "./_components/ui/input"
+import Image from "next/image";
 
 const Home = () => {
     return (
+        <div>
+            <Header />
+            <div className="p-5">
+                <h2 className="text-xl font-bold">Olá,Mario Luis!</h2>
+                <p>Segunda-Feira 5 Agosto.</p>
 
-        <Button>test</Button>
+                <div className="mt-6 flex gap-2 items-center">
+                    <Input placeholder=" Buascar..." />
+                    <Button>
+                        <SearchIcon />
+                    </Button>
+                </div>
+                
+                <div className="relative h-[150px] w-full mt-5">
+                    <Image alt="baner,agende nos melhores com fsw-barber" src="/banner-01.png" fill className="object-cover rounded-xl" />
+                </div>
+            </div>
+        </div>
     )
 }
 
