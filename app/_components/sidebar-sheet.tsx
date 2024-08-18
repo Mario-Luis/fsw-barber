24d92d2f8dbe8f3eced687d743614c0041162d45
Mario Luis <yamamotomarioluis@gmail.com>
2024-08-18 20:14:03 +0900

    feat: add Google login setup

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,15 +1,24 @@
-import { Calendar, CalendarIcon, HomeIcon, Import, LogOutIcon, MenuIcon } from "lucide-react";
+"use client"
+
+import { Calendar, CalendarIcon, HomeIcon, Import, LogInIcon, LogOutIcon, MenuIcon } from "lucide-react";
 import { SheetTrigger,Sheet, SheetContent, SheetTitle, SheetHeader, SheetClose } from "./ui/sheet";
 import { Button } from "./ui/button";
 import quickSearchOptions from "../_constants/search";
 import Image from "next/image";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
+import { Dialog, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from "./ui/dialog";
+import { DialogHeader } from "./ui/dialog";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 
 
 
 const SidebarSheet = () => {
+    const {data} = useSession()
+    const handleLoginWithGoogleleClick = () => signIn ("google")
+    const handleLogoutClick = () => signOut()
+
     return ( 
         <Sheet >
             <SheetTrigger asChild>
@@ -17,18 +26,45 @@ const SidebarSheet = () => {
                     <MenuIcon className=" text-primary"/>
                 </Button>
             </SheetTrigger>
-            <SheetContent className=" max-w-[250px] overflow-y-auto">
+            <SheetContent className=" max-w-[280px] overflow-y-auto">
                 <SheetHeader>
                     <SheetTitle className="text-left">Menu</SheetTitle>
                 </SheetHeader>
                 <div className=" gap-2 py-5 border-b border-solid flex items-center">
-                    <Avatar>
-                        <AvatarImage  src="/IMG_7561.jpg"/>
-                    </Avatar>
-                    <div>
-                        <p className=" font-bold">Mario Luis</p>
-                        <p className=" text-sm">marioluis@gmaiil.com</p>
-                    </div>
+                    {data?.user? (
+                        <div className=" flex items-center gap-2">
+                            <Avatar>
+                                <AvatarImage  src={data?.user.image ?? ""}/>
+                            </Avatar>
+                            <div>
+                                <p className=" font-bold">{data?.user?.name}</p>
+                                <p className=" text-xs">{data?.user?.email}</p>
+                            </div>
+                        </div>
+                    ) :(
+                        <>
+                        <Dialog>
+                            <DialogTrigger asChild>
+                                <Button variant="ghost" className=" justify-start gap-1">
+                                    <LogInIcon size={25}/>
+                                    Fazer login
+                                </Button>
+                            </DialogTrigger>
+                            <DialogContent className=" max-w-[270px]">
+                                    <DialogHeader>
+                                        <DialogTitle>Fazer login</DialogTitle>
+                                            <DialogDescription>
+                                            Logar com Google
+                                        </DialogDescription>
+                                    </DialogHeader>
+                            <Button variant="outline" className=" gap-1" onClick={handleLoginWithGoogleleClick} >
+                                <Image alt="Logar com Google" src="/icons8-google-94.png" height={18} width={18}/>
+                                Google
+                            </Button>
+                            </DialogContent>
+                        </Dialog>
+                        </>
+                    )}
                 </div>
                 <div className=" flex flex-col gap-1 py-3 border-b border-solid ">
                     <SheetClose asChild>
@@ -53,7 +89,7 @@ const SidebarSheet = () => {
                     ))}
                 </div>
                 <div className=" flex flex-col gap-1 py-3 border-b border-solid ">
-                    <Button variant="ghost" className=" gap-1 justify-start" >
+                    <Button variant="ghost" className=" gap-1 justify-start" onClick={handleLogoutClick} >
                         <LogOutIcon size={18}/>
                         Sair da conta                        
                     </Button>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "./_components/ui/sonner";
 import FooterItem from "./_components/footer-item";
+import AuthProvider from "./_providers/auth";
 
 
 
@@ -20,9 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}
-      <Toaster />
-      <FooterItem/>
+      <body className={inter.className}>
+        <AuthProvider>
+          {children}
+          <Toaster />
+          <FooterItem/>
+        </AuthProvider>
       </body>
     </html>
   );
